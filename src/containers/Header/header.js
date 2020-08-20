b185e6b3ee3cfcf323319931b98c4c4249c7a873
Xavier Colombel <xavier@lereacteur.io>
2020-08-20 23:17:26 +0200

    update layout

--- src/containers/Header/header.js
+++ src/containers/Header/header.js
@@ -1,71 +1,108 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useParams
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
 } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FolderIcon from '@material-ui/icons/Folder';
-import TextField from '@material-ui/core/TextField';
-import '../Header/style.css'
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import FolderIcon from "@material-ui/icons/Folder";
+import TextField from "@material-ui/core/TextField";
+import "../Header/style.css";
 
 const useStyles = makeStyles((theme) => ({
-    bottomNavigationRoot: {
-        width: 500,
-        backgroundColor: 'transparent',
+  bottomNavigationRoot: {
+    width: 500,
+    backgroundColor: "transparent",
+  },
+  textFieldRoot: {
+    "& > *": {
+      /* margin: theme.spacing(1), */
+      margin: "50px auto auto auto",
+      width: "400px",
+      backgroundColor: "rgba(0,0,0,0.5)",
     },
-    textFieldRoot: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: '25ch',
-        },
-    },
+  },
+  cssOutlinedInput: {
+    color: "#fc00fd",
+  },
+  notchedOutline: {
+    borderWidth: "2px",
+    borderColor: "#fc00fd !important",
+    color: "#fc00fd",
+  },
+  label: {
+    color: "#fc00fd !important",
+  },
 }));
 
-
 const Header = () => {
+  const classes = useStyles();
+  const [value, setValue] = useState("category");
 
-    const classes = useStyles();
-    const [value, setValue] = useState('category');
+  return (
+    <div className="header">
+      <div className="gradient">
+        <div className="title">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <h1>Funko Pop Collection</h1>
+          </Link>
 
-    const inputProps = {
-        classes: {
-            input: {
-                borderColor: 'green',
-            }
-        }
-    };
-
-    return (
-        <div className='header'>
-            <div className='title'>
-                <Link to='/' style={{ textDecoration: 'none' }}>
-                    <h1>Funko Pop Collection</h1>
-                </Link>
-
-                <BottomNavigation
-                    value={value}
-                    onChange={(event, newValue) => {
-                        setValue(newValue);
-                    }}
-                    showLabels
-                    className={classes.bottomNavigationRoot}
-                    style={{ justifyContent: 'flex-end' }}
-                >
-                    <BottomNavigationAction label="Category" value="category" icon={<FolderIcon />} />
-                    <BottomNavigationAction label="Collection" value="collection" icon={<FavoriteIcon />} />
-                </BottomNavigation>
-            </div>
-            <form className={classes.textFieldRoot} noValidate autoComplete="off" style={{ display: 'flex', justifyContent: 'start' }}>
-                <TextField id="outlined-basic" label="Search" variant="outlined" inputProps={inputProps} />
-            </form>
+          <BottomNavigation
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            showLabels
+            className={classes.bottomNavigationRoot}
+            style={{ justifyContent: "flex-end" }}
+          >
+            <BottomNavigationAction
+              label="Category"
+              value="category"
+              icon={<FolderIcon />}
+            />
+            <BottomNavigationAction
+              label="Collection"
+              value="collection"
+              icon={<FavoriteIcon />}
+            />
+          </BottomNavigation>
         </div>
-    )
-}
+        <form
+          className={classes.textFieldRoot}
+          noValidate
+          autoComplete="off"
+          style={{ display: "flex", justifyContent: "start" }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+            InputProps={{
+              classes: {
+                root: classes.cssOutlinedInput,
+                notchedOutline: classes.notchedOutline,
+                focused: classes.cssFocused,
+              },
+              inputMode: "numeric",
+            }}
+            InputLabelProps={{
+              classes: {
+                root: classes.label,
+                /* focused: classes.focusedLabel,
+              error: classes.erroredLabel, */
+              },
+            }}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
