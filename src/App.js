e5df217c81a3688b96950fbb5386812a74bb6404
Xavier Colombel <xavier@lereacteur.io>
2020-08-20 23:27:07 +0200

    update links

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import Header from './containers/Header/header';
-import Footer from './containers/Footer';
-import HomePage from './containers/HomePage';
-import FunkosList from './components/FunkosList';
-import Container from '@material-ui/core/Container';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./containers/Header/header";
+import Footer from "./containers/Footer";
+import HomePage from "./containers/HomePage";
+import FunkosList from "./components/FunkosList";
+import Container from "@material-ui/core/Container";
 
 function App() {
   return (
